68291b9f0d7e93369aa088a9564616990a400d21
michaeldel <michaeldel@protonmail.com>
2021-04-10 21:09:55 +0200

    Add variables to outline

--- outline.py
+++ outline.py
@@ -13,11 +13,20 @@ class TasksFile:
     def name(self) -> str:
         return self.path.stem
 
+@dataclass
+class VarsFile:
+    path: pathlib.Path
+
+    @property
+    def name(self) -> str:
+        return self.path.stem
+
 
 @dataclass
 class Role:
     name: str
     tasksfiles: list[TasksFile] = field(default_factory=list)
+    varsfiles: list[VarsFile] = field(default_factory=list)
 
 
 roles = {}
@@ -31,9 +40,25 @@ for path in sorted(pathlib.Path().glob('roles/**/tasks/*.yml')):
         roles[rolename] = Role(name=rolename)
     roles[rolename].tasksfiles.append(taskfile)
 
+for path in sorted(pathlib.Path().glob('roles/**/vars/*.yml')):
+    varsfile = VarsFile(path=path)
+
+    rolename = path.parent.parent.stem
+
+    if rolename not in roles:
+        roles[rolename] = Role(name=rolename)
+    roles[rolename].varsfiles.append(varsfile)
+
 
 for role in roles.values():
     print("👥", role.name)
+
+    for varsfile in role.varsfiles:
+        print("  💾", varsfile.name)
+
+        with open(varsfile.path) as f:
+            for variable in yaml.load(f, Loader=yaml.CLoader):
+                print("    -", variable)
 
     for tasksfile in role.tasksfiles:
         print("  📝", tasksfile.name)
@@ -46,4 +71,3 @@ for role in roles.values():
                     print("    - 👮 assert")
                 if 'name' in block:
                     print("    -", block['name'])
-                raise NotImplementedError
